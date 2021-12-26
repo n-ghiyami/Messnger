e7f3f9d9704b9de6612c884d8b8913bed73a2d9c
=narges <=n.ghiyami@gmail.com>
2021-12-27 00:49:15 +0330

    second phase-2 2.1.0

--- Message_directory_module.py
+++ Message_directory_module.py
@@ -12,12 +12,11 @@ class Homepage:
     load_homepage
     load_all_messages_from_directory : load messges from inbox/sentbox or draft
     create_new_message
-    logout
+
     """
 
     def __init__(self, path):
         self.path = path
-        self.homepage_list = []
 
     def load_homepage(self):
         """
@@ -25,20 +24,19 @@ class Homepage:
         """
         if self.path != '':
             directory_contents = os.listdir(self.path)
-            print(directory_contents)
+            return (directory_contents)
 
-    def load_all_messages_from_directory(self):
+    def load_all_messages_from_directory(self,directory_name):
         """
         This method loads all messages list from any given directory including inbox, sentbox or draft
         """
-        all_messages = pd.read_csv(f"{self.path}/index_file")
-        print(f"total messages:{all_messages.index}\n"
-              f"{x+1} sender: {all_messages.loc[x,'sender']} "
+        all_messages = pd.read_csv(f"{os.getcwd()}/{directory_name}/index_file")
+        print(f"total messages:{all_messages.index}\n",
+              (f"{x+1} sender: {all_messages.loc[x,'sender']} "
               f"receiver {all_messages.loc[x,'receiver']} "
-              f"title: {all_messages.loc[x, 'title']} "
-              f"time: {all_messages.loc[x, 'time']}"
-              f"read status: {all_messages.loc[x, 'read_status']}"
-              for x in all_messages.index)
+              f"unique_id: {all_messages.loc[x, 'unique_id']} "
+              f"read status: {all_messages.loc[x, 'read_status']}" if directory_name == 'Inbox' else ''
+              for x in all_messages.index))
 
     def create_new_message(self , input_text , receiver_address,sender_address):
         """
@@ -47,12 +45,85 @@ class Homepage:
         new_message = Message.add_message(input_text,receiver_address,sender_address)
         return new_message
 
+    def save_in_draft(self,temp_message):
+        print("this method saves a specific message in draft")
+        if self.path != '':
+            path = f"{os.getcwd()}/Draft"
+            fl = File_Handler(f"{path}/{temp_message.title}_{temp_message.time}.csv")
+            fl.write(dict(temp_message))
+            fl = File_Handler(f"{path}/index_file.csv")
+            unique_id = f"{temp_message.title}_{temp_message.time}"
+            content = fl.read()
+            new_record = {'sender':temp_message.sender_address,'receiver':temp_message.receiver_address,
+                          'unique_id':unique_id }
+            content.append(new_record)
+            fl.write(content)
+
+    def show_message(self, path, message_number):
+        """
+        this method shows a specific message
+        :param message_number:
+        :return:
+        """
+        df = pd.read_csv(f"{path}/index_file")
+        message_path = f"{path}/{df.loc[message_number-1 , 'unique_id']}.csv"
+        message_df = pd.read_csv(message_path)
+        message = Message.add_message(message_df.loc[0,'text'],
+            message_df.loc[0,'receiver'],message_df.loc[0,'sender_address'],
+            message_df.loc[0,'title'],message_df.loc[0,'time'])
+        if path.endswith('Inbox'):
+            df.loc[message_number-1 , 'read_status'] = 'read'
+        df.to_csv(f"{path}/index_file")
+        return str(message)
+
+    def delete_message(self,path, message_number):
+        """
+        this method deletes a specific message from path folder
+        :param path:
+        :param message_number:
+        :return:
+        """
+        df = pd.read_csv(f"{path}/index_file.csv")
+        os.remove(f"{path}/{df.loc[message_number-1 , 'unique_id']}")
+        df.drop(labels=message_number-1,axis=0)
+        print("Message has just been deleted!")
+
+    def send_message(self,temp_message):
+        if self.path != '':
+            path = f"{os.getcwd()}/Sentbox"
+            unique_id = f"{temp_message.title}_{temp_message.time}"
+            fl = File_Handler(f"{path}/{unique_id}.csv")
+            fl.write(dict(temp_message))
+            fl = File_Handler(f"{path}/index_file.csv")
+            content = fl.read()
+            new_record = {'sender':temp_message.sender_address,'receiver':temp_message.receiver_address,
+                          'unique_id':unique_id }
+            content.append(new_record)
+            fl.write(content)
+#           update receiver inbox
+            os.chdir("..") #change directory to all users directory
+            path = f"{os.getcwd()}/{temp_message.receiver_address}/Inbox"
+            fl = File_Handler(f"{path}/{unique_id}_{temp_message.sender_address}.csv")
+            # may some users send message simultaneously
+            fl.write(dict(temp_message))
+            fl = File_Handler(f"{path}/index_file.csv")
+            content = fl.read()
+            new_record = {'sender': temp_message.sender_address, 'receiver': temp_message.receiver_address,
+                          'unique_id': unique_id, 'read_status':'unread'}
+            content.append(new_record)
+            fl.write(content)
+
+
+
+
 class Message:
     """
     attributes:
     text
     receiver_address
     sender_address
+    title
+    time
 
     methods:
     check_address():checks whether receiver address exists
@@ -85,6 +156,39 @@ class Message:
     def __str__(self):
         return f" sender: {self.sender_address} , receiver: {self.receiver_address}," \
                f" title: {self.title}, time: {self.time} \n{self.text}"
+
+    def __dict__(self):
+        return {'sender': f'{self.sender_address}' , 'receiver': f'{self.receiver_address}',\
+               'title': f'{self.title}', 'time': f'{self.time}', 'text': f'{self.text}'}
+
+class Recieved_message(Message):
+    def __init__(self):
+        super().__init__()
+        self.read_status = 'unread'
+
+    def make_reply_message(self):
+        print("this method replys a specific message to sender")
+
+    def make_forward_message(self, to_username):
+        print("this method forwards a specific message")
+
+class Sent_message(Message):
+    def __init__(self):
+        super().__init__()
+
+    def make_forward_message(self, to_username):
+        print("this method forwards a specific message")
+
+class Draft_message(Message):
+    def __init__(self):
+        super().__init__()
+
+    def switch_to_edit_mode(self, message_number):
+        print("this method opens a specific message and let us edit it")
+
+    def send_massage(self):
+        print("this method send a specific message to a username and remove it from draft")
+
 
 class Box:
     """
@@ -127,40 +231,46 @@ class Box:
         :return:
         """
         df = pd.read_csv(f"{path}/index_file.csv")
+        os.remove(f"{path}/{df.loc[message_number-1 , 'unique_id']}")
         df.drop(labels=message_number-1,axis=0)
         print("Message has just been deleted!")
+#
+# class Inbox(Box):
+#     def __init__(self):
+#         super().__init__()
+#
+#
+#
+#     def reply_message(self):
+#         print("this method replys a specific messageto sender")
+#
+#     def forward_message(self, to_username):
+#         print("this method forwards a specific message")
+#
+#
+# class Sentbox(Box):
+#     def __init__(self):
+#         super().__init__()
+#
+#     def forward_message(self, to_username):
+#         print("this method forwards a specific message")
 
-
-class Inbox(Box):
-    def __init__(self):
-        super().__init__()
-
-    def save_in_draft(self, ):
-        print("this method saves a specific message in draft")
-
-    def reply_message(self):
-        print("this method replys a specific messageto sender")
-
-    def forward_message(self, to_username):
-        print("this method forwards a specific message")
-
-
-class Sentbox(Box):
-    def __init__(self):
-        super().__init__()
-
-    def forward_message(self, to_username):
-        print("this method forwards a specific message")
-
-
-class Draft(Box):
-    def __init__(self):
-        super().__init__()
-
-    def switch_to_edit_mode(self, message_number):
-        print("this method opens a specific message and let us edit it")
-
-    def send_message(self, to_username):
-        print("this method send a specific message to a username and remove it from draft")
-
-
+#
+# class Draft(Box):
+#     def __init__(self):
+#         super().__init__()
+#
+#     def switch_to_edit_mode(self, message_number):
+#         print("this method opens a specific message and let us edit it")
+#
+#     def send_message(self, to_username):
+#         print("this method send a specific message to a username and remove it from draft")
+#
+#     def save_in_draft(self , path , temp_message):
+#         """
+#         :params:
+#         :return:
+#         """
+#
+#         fl = File_Handler(f"{os.getcwd()}/Draft/{temp_message.sender}")
+#         fl.write(dict(temp_message))
